0e541990a5f125c8ee345dab37e36f1b8f35e132
cjhaug <ch@chaug.de>
2015-09-04 07:15:45 +0200

    Job-DSL: SvnCommitContext

--- src/main/java/jenkins/plugins/svn_commit/SvnCommitDslExtension.java
+++ src/main/java/jenkins/plugins/svn_commit/SvnCommitDslExtension.java
@@ -1,6 +1,7 @@
 package jenkins.plugins.svn_commit;
 
 import hudson.Extension;
+import javaposse.jobdsl.dsl.Context;
 import javaposse.jobdsl.dsl.helpers.publisher.PublisherContext;
 import javaposse.jobdsl.plugin.ContextExtensionPoint;
 import javaposse.jobdsl.plugin.DslExtensionMethod;
@@ -11,4 +12,26 @@ public class SvnCommitDslExtension extends ContextExtensionPoint {
 	public Object svncommit(String commitComment, boolean includeIgnored) {
 		return new SvnCommitPublisher(commitComment, includeIgnored);
 	}
+
+	@DslExtensionMethod(context = PublisherContext.class)
+	public Object svncommit(Runnable closure) {
+		SvnCommitContext context = new SvnCommitContext();
+		executeInContext(closure, context);
+		return new SvnCommitPublisher(context.commitComment,
+				context.includeIgnored);
+	}
+
+	public class SvnCommitContext implements Context {
+		String commitComment;
+		boolean includeIgnored = false;
+
+		public void commitComment(String value) {
+			commitComment = value;
+		}
+
+		public void includeIgnored(boolean value) {
+			includeIgnored = value;
+		}
+	}
+
 }
